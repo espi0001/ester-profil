6383de6c3712e16d9cdb6681690002865a55e82e
Ester <espi0001@stud.kea.dk>
2024-09-26 13:04:35 +0200

    data og iconer

---
+++ src/data/card-content.js
@@ -0,0 +1,33 @@
+export const cardContent = [
+  {
+    image: "../images/barselona.png",
+    title: "Barselona",
+    body: "For my exam project my group and I developed a website for the music group Barselona. Focusing on creating and communicating a solution that reflects Barcelona’s visual identity, enhances their concept, and engages the target audience effectively.",
+    link: "https://praktik--eksamen-barselona-ester.netlify.app/",
+  },
+  {
+    image: "../images/pizza-madness.png",
+    title: "Pizza Madness",
+    body: "Animation project for Pizza Madness",
+    link: "https://pizza-madness.netlify.app/",
+  },
+  {
+    image: "../images/girl-talk.png",
+    title: "Girl Talk",
+    body: "For GirlTalk, a Danish NGO focused on preventing distress in young women, we designed a one-page web feature highlighting their parental counseling service. ",
+    link: "https://girltalkteam16.netlify.app/",
+    // link-text: "Link to the project",
+  },
+  {
+    image: "../images/cph-light.png",
+    title: "Copenhagen Light Festival",
+    body: "For Copenhagen Light Festival, an outdoor light festival featuring light installations around the city, we designed a website aimed to reflect the festival’s atmosphere.",
+    link: "https://cfl-team-19.netlify.app/",
+  },
+  {
+    image: "../images/vild-mad.png",
+    title: "Vild Mad",
+    body: "For Supertusch, a creative digital agency, we developed a theme-site targeting 7th graders to teach them about foraging in their cooking classes, focusing on seasonal ingredients and forest finds.",
+    link: "https://espi0001.github.io/vildmadsite/",
+  },
+];
